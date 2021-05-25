cdf4928366bb231f3840350f8607019439c451c2
lee17kai <81439325+lee17kai@users.noreply.github.com>
2021-05-25 08:40:26 -0500

    0840

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,15 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyDdE-k25JE8RpZLQ3zWQIASq94lYcAs9n8",
+  authDomain: "kiei-451-33157.firebaseapp.com",
+  databaseURL: "https://kiei-451-33157-default-rtdb.firebaseio.com",
+  projectId: "kiei-451-33157",
+  storageBucket: "kiei-451-33157.appspot.com",
+  messagingSenderId: "167630194199",
+  appId: "1:167630194199:web:14694fc8dba04e37283e52"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
